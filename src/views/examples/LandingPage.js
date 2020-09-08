2650a728b0afbbcbe12d11784383f67bbcf62b8d
Khemy543 <gassafuah@gmail.com>
2020-09-08 13:23:51 +0000

    new label

--- src/views/examples/LandingPage.js
+++ src/views/examples/LandingPage.js
@@ -121,8 +121,161 @@ class LandingPage extends React.Component {
               </Row>
             </div>
           </div>
-          <section className="section section-lg">
-            <section className="section">
+          <section className="section section-lg section-coins" style={{marginTop:"-50px"}}>
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path3.png")}
+            />
+            <Container>
+              <Row>
+                <Col md="4">
+                  <hr className="line-info" />
+                  <h1>
+                    Select an artist{" "}
+                    <span className="text-info">that you want to view</span>
+                  </h1>
+                </Col>
+              </Row>
+              <Row>
+              {data.map((value,key)=>(
+                <Col md="4" key={key}>
+                  <Card className="card-coin card-plain">
+                    <CardHeader>
+                      <img
+                        alt="..."
+                        className="img-center img-fluid"
+                        src={value.profile}
+                        style={{borderRadius:"50%"}}
+                      />
+                    </CardHeader>
+                    <CardBody>
+                      <Row>
+                        <Col className="text-center" md="12">
+                          <h4 className="text-uppercase">{value.name}</h4>
+                          <span>{value.music_type}</span>
+                          <hr className="line-primary" />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <ListGroup>
+                          <ListGroupItem>{value.no_of_tracks} tracks available</ListGroupItem>
+                        </ListGroup>
+                      </Row>
+                    </CardBody>
+                    <CardFooter className="text-center">
+                      <Button className="btn-simple" color="primary" onClick={()=>this.props.history.push("/profile-page",{id:value.id})}>
+                        View Artist
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              ))}
+              </Row>
+              <br />
+                      <a
+                        className="font-weight-bold text-info mt-5"
+                        href="/allartist-page"
+                      >
+                        Show all{" "}
+                        <i className="tim-icons icon-minimal-right text-info" />
+                      </a>
+            </Container>
+          </section>
+          <section className="section section-lg section-safe" style={{marginTop:"20px"}}>
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path5.png")}
+            />
+            <Container>
+              <Row className="row-grid justify-content-between">
+                <Col md="5">
+                  <img
+                    alt="..."
+                    className="img-fluid floating"
+                    src={require("assets/img/head2.jpeg")}
+                  />
+                  <Card className="card-stats bg-danger">
+                    <CardBody>
+                      <div className="justify-content-center">
+                        <div className="numbers">
+                          <CardTitle tag="p">100%</CardTitle>
+                          <p className="card-category text-white">Safe</p>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                  <Card className="card-stats bg-info">
+                    <CardBody>
+                      <div className="justify-content-center">
+                        <div className="numbers">
+                          <CardTitle tag="p">573 K</CardTitle>
+                          <p className="card-category text-white">
+                            Satisfied customers
+                          </p>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                  <Card className="card-stats bg-default">
+                    <CardBody>
+                      <div className="justify-content-center">
+                        <div className="numbers">
+                          <CardTitle tag="p">10 425</CardTitle>
+                          <p className="card-category text-white">Business</p>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="6">
+                  <div className="px-md-5">
+                    <hr className="line-success" />
+                    <h3>Awesome Music</h3>
+                    <p>
+                      The design system comes with three pre-built pages to help
+                      you get started faster. You can change the text and images
+                      and you're good to go.
+                    </p>
+                    <ul className="list-unstyled mt-5">
+                      <li className="py-2">
+                        <div className="d-flex align-items-center">
+                          <div className="icon icon-success mb-2">
+                            <i className="tim-icons icon-vector" />
+                          </div>
+                          <div className="ml-3">
+                            <h6>Carefully crafted components</h6>
+                          </div>
+                        </div>
+                      </li>
+                      <li className="py-2">
+                        <div className="d-flex align-items-center">
+                          <div className="icon icon-success mb-2">
+                            <i className="tim-icons icon-tap-02" />
+                          </div>
+                          <div className="ml-3">
+                            <h6>Amazing page examples</h6>
+                          </div>
+                        </div>
+                      </li>
+                      <li className="py-2">
+                        <div className="d-flex align-items-center">
+                          <div className="icon icon-success mb-2">
+                            <i className="tim-icons icon-single-02" />
+                          </div>
+                          <div className="ml-3">
+                            <h6>Super friendly support team</h6>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>    
+            <section className="section" style={{marginTop:"-50px"}}>
               <img
                 alt="..."
                 className="path"
@@ -240,161 +393,6 @@ class LandingPage extends React.Component {
                 </Row>
               </Container>
             </section>
-          </section>
-          <section className="section section-lg section-coins" style={{marginTop:"-50px"}}>
-            <img
-              alt="..."
-              className="path"
-              src={require("assets/img/path3.png")}
-            />
-            <Container>
-              <Row>
-                <Col md="4">
-                  <hr className="line-info" />
-                  <h1>
-                    Select an artist{" "}
-                    <span className="text-info">that you want to view</span>
-                  </h1>
-                </Col>
-              </Row>
-              <Row>
-              {data.map((value,key)=>(
-                <Col md="4" key={key}>
-                  <Card className="card-coin card-plain">
-                    <CardHeader>
-                      <img
-                        alt="..."
-                        className="img-center img-fluid"
-                        src={value.profile}
-                        style={{borderRadius:"50%"}}
-                      />
-                    </CardHeader>
-                    <CardBody>
-                      <Row>
-                        <Col className="text-center" md="12">
-                          <h4 className="text-uppercase">{value.name}</h4>
-                          <span>{value.music_type}</span>
-                          <hr className="line-primary" />
-                        </Col>
-                      </Row>
-                      <Row>
-                        <ListGroup>
-                          <ListGroupItem>{value.no_of_tracks} tracks available</ListGroupItem>
-                        </ListGroup>
-                      </Row>
-                    </CardBody>
-                    <CardFooter className="text-center">
-                      <Button className="btn-simple" color="primary" onClick={()=>this.props.history.push("/profile-page",{id:value.id})}>
-                        View Artist
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </Col>
-              ))}
-              </Row>
-              <br />
-                      <a
-                        className="font-weight-bold text-info mt-5"
-                        href="/allartist-page"
-                      >
-                        Show all{" "}
-                        <i className="tim-icons icon-minimal-right text-info" />
-                      </a>
-            </Container>
-          </section>
-          <section className="section section-lg section-safe">
-            <img
-              alt="..."
-              className="path"
-              src={require("assets/img/path5.png")}
-            />
-            <Container>
-              <Row className="row-grid justify-content-between">
-                <Col md="5">
-                  <img
-                    alt="..."
-                    className="img-fluid floating"
-                    src={require("assets/img/head2.jpeg")}
-                  />
-                  <Card className="card-stats bg-danger">
-                    <CardBody>
-                      <div className="justify-content-center">
-                        <div className="numbers">
-                          <CardTitle tag="p">100%</CardTitle>
-                          <p className="card-category text-white">Safe</p>
-                        </div>
-                      </div>
-                    </CardBody>
-                  </Card>
-                  <Card className="card-stats bg-info">
-                    <CardBody>
-                      <div className="justify-content-center">
-                        <div className="numbers">
-                          <CardTitle tag="p">573 K</CardTitle>
-                          <p className="card-category text-white">
-                            Satisfied customers
-                          </p>
-                        </div>
-                      </div>
-                    </CardBody>
-                  </Card>
-                  <Card className="card-stats bg-default">
-                    <CardBody>
-                      <div className="justify-content-center">
-                        <div className="numbers">
-                          <CardTitle tag="p">10 425</CardTitle>
-                          <p className="card-category text-white">Business</p>
-                        </div>
-                      </div>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col md="6">
-                  <div className="px-md-5">
-                    <hr className="line-success" />
-                    <h3>Awesome Music</h3>
-                    <p>
-                      The design system comes with three pre-built pages to help
-                      you get started faster. You can change the text and images
-                      and you're good to go.
-                    </p>
-                    <ul className="list-unstyled mt-5">
-                      <li className="py-2">
-                        <div className="d-flex align-items-center">
-                          <div className="icon icon-success mb-2">
-                            <i className="tim-icons icon-vector" />
-                          </div>
-                          <div className="ml-3">
-                            <h6>Carefully crafted components</h6>
-                          </div>
-                        </div>
-                      </li>
-                      <li className="py-2">
-                        <div className="d-flex align-items-center">
-                          <div className="icon icon-success mb-2">
-                            <i className="tim-icons icon-tap-02" />
-                          </div>
-                          <div className="ml-3">
-                            <h6>Amazing page examples</h6>
-                          </div>
-                        </div>
-                      </li>
-                      <li className="py-2">
-                        <div className="d-flex align-items-center">
-                          <div className="icon icon-success mb-2">
-                            <i className="tim-icons icon-single-02" />
-                          </div>
-                          <div className="ml-3">
-                            <h6>Super friendly support team</h6>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </section>
           <section className="section">
             <Container>
               <Row>
@@ -409,14 +407,14 @@ class LandingPage extends React.Component {
                           <Col md="6">
                             <FormGroup>
                               <label>Your Name</label>
-                              <Input defaultValue="Mike" type="text" />
+                              <Input placeholder="name" type="text" />
                             </FormGroup>
                           </Col>
                           <Col md="6">
                             <FormGroup>
                               <label>Email address</label>
                               <Input
-                                placeholder="mike@email.com"
+                                placeholder="email"
                                 type="email"
                               />
                             </FormGroup>
@@ -426,7 +424,7 @@ class LandingPage extends React.Component {
                           <Col md="6">
                             <FormGroup>
                               <label>Phone</label>
-                              <Input defaultValue="001-12321345" type="text" />
+                              <Input placeholder="phone" type="text" />
                             </FormGroup>
                           </Col>
                           <Col md="6">
